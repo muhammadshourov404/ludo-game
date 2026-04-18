98984a4e26c44ec0b2feb7279dc71c7d97b2f24e
Muhammad Shourov (V4MPIR3) <muhammad.shourov01@gmail.com>
2026-04-19 00:37:51 +0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,32 @@
-const CACHE_NAME = 'ludo-v1';
-const ASSETS = [
-    './', './index.html', './css/style.css', './css/animations.css',
-    './js/board.js', './js/game.js', './js/sound.js', './js/cards.js', 
-    './js/ai.js', './js/network.js', './manifest.json'
+const CACHE_NAME = 'ludo-royale-v1';
+const urlsToCache = [
+  '/ludo-game/',
+  '/ludo-game/index.html',
+  '/ludo-game/css/style.css',
+  '/ludo-game/css/themes.css',
+  '/ludo-game/css/animations.css',
+  '/ludo-game/js/main.js',
+  '/ludo-game/js/game.js',
+  '/ludo-game/js/board.js',
+  '/ludo-game/js/ai.js',
+  '/ludo-game/js/network.js',
+  '/ludo-game/js/cards.js',
+  '/ludo-game/js/sound.js',
+  '/ludo-game/manifest.json',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css',
+  'https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600;700&display=swap'
 ];
 
-self.addEventListener('install', (e) => {
-    e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
 });
 
-self.addEventListener('fetch', (e) => {
-    e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
 });
