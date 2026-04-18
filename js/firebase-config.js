a500d88a3884e397f484edc73259711b874b9371
Muhammad Shourov (V4MPIR3) <muhammad.shourov01@gmail.com>
2026-04-19 00:44:15 +0600

    Add Firebase configuration file
    Added Firebase configuration for the project.

---
+++ js/firebase-config.js
@@ -0,0 +1,11 @@
+// Firebase configuration
+export const firebaseConfig = {
+  apiKey: "AIzaSyDhAhSJhg9cAefOC1qg0VNfaLxqBX0-Itk",
+  authDomain: "ludo-game-fd743.firebaseapp.com",
+  databaseURL: "https://ludo-game-fd743-default-rtdb.firebaseio.com",
+  projectId: "ludo-game-fd743",
+  storageBucket: "ludo-game-fd743.firebasestorage.app",
+  messagingSenderId: "730048485099",
+  appId: "1:730048485099:web:e897a332370427f6de50ae",
+  measurementId: "G-GEG3CKBYWP"
+};
