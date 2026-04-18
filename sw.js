a3fc25d306852e204a3f61ecd31e13ace8f0ad7d
Muhammad Shourov (V4MPIR3) <muhammad.shourov01@gmail.com>
2026-04-18 23:47:31 +0600

    Refactor service worker code for clarity

--- sw.js
+++ sw.js
@@ -1,30 +1,14 @@
-// sw.js - Service Worker by V4MPIR3
 const CACHE_NAME = 'ludo-v1';
 const ASSETS = [
-    './',
-    './index.html',
-    './css/style.css',
-    './css/themes.css',
-    './css/animations.css',
-    './js/board.js',
-    './js/game.js',
-    './js/sound.js',
-    './js/cards.js',
-    './js/ai.js',
-    './js/network.js',
-    './manifest.json'
+    './', './index.html', './css/style.css', './css/animations.css',
+    './js/board.js', './js/game.js', './js/sound.js', './js/cards.js', 
+    './js/ai.js', './js/network.js', './manifest.json'
 ];
 
-// ইন্সটল করার সময় ফাইলগুলো সেভ করা
 self.addEventListener('install', (e) => {
-    e.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-    );
+    e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
 });
 
-// অফলাইনে ফাইলগুলো সার্ভ করা
 self.addEventListener('fetch', (e) => {
-    e.respondWith(
-        caches.match(e.request).then((res) => res || fetch(e.request))
-    );
+    e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
 });
